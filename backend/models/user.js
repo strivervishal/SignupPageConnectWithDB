c657eb878485a6752f111942fcf1cd1f8997cb5b
strivervishal <strivervishal@gmail.com>
2025-01-21 14:09:40 +0530

    Revert "fix issues"
    This reverts commit 8e1af277359e4b15bbee3f3d79c74b92006e9eea.

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,27 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  photo: { type: String, required: false },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String, // This will store the path or URL of the uploaded photo
+    required: false, // Make it optional if you don't want to enforce it during registration
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const authRoutes = require("./routes/auth");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
@@ -15,14 +14,17 @@ app.get("/", (req, res) => res.send("Welcome to the Signup & Login API"));
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://vp564141:h4qT5DhEbayT1gE5@signuppage.en9tg.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("Database connection error:", err));
 
 // Start the Server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
 });
